2879d5b38e579ccf17084f4a69ffe947d5f87be4
HemaAmruthaDasari <137883961+HemaAmruthaDasari@users.noreply.github.com>
2023-07-19 10:49:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function fruits()
+{
+    document.getElementById("fruits").src="https://e0.pxfuel.com/wallpapers/419/782/desktop-wallpaper-tom-and-jerry-phone-top-tom-and-jerry-phone-tom-and-jerry-iphone-tom-and-jerry-christmas.jpg";
+}
+function flowers()
+{
+    document.getElementById("fruits").src="https://im.idiva.com/content/2022/Jul/inline-doraemon--Disney-Hotstar_62df7e915db15.jpg";
+
+}
+function birds()
+{
+     document.getElementById("fruits").src="https://e0.pxfuel.com/wallpapers/264/307/desktop-wallpaper-little-krishna-full-size-little-cute-krishna.jpg";
+}
